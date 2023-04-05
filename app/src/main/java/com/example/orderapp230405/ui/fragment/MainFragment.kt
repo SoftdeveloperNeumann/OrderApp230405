9be62159b00f02fe7e36f20b908dc8b8d1e2e861
Frank <f.neumann.berlin@googlemail.com>
2023-04-05 09:12:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/orderapp230405/ui/fragment/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.example.orderapp230405.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.orderapp230405.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+
+}
